c84032225f52ca7e58954856df0cd4dc57a36803
aileeeeen <98327449+aileeeeen@users.noreply.github.com>
2024-08-18 17:56:31 +0200

    Update About.js
    changes

--- src/components/About.js
+++ src/components/About.js
@@ -55,7 +55,7 @@ function About(props) {
                     Throughout my studies, I conducted research projects focused on shorebirds and marine mammals, while developing a strong interest in science dissemination by creating videos and other contents for social media.
                 </Paragraph>
                 <Paragraph>
-                   Currently, I am working as a communication and social media manager for a non-profit organization dedicated to the effective conservation of Italian seas through innovative outreach and conservation projects.
+                   Currently, I am working as a communication and outreach manager for a non-profit organization dedicated to the effective conservation of Italian seas through innovative outreach and conservation projects.
                 </Paragraph>
                 <Paragraph>
                     I am moved by a strong desire to learn and get involved by always giving my best effort. Some of my distinguishing skills include self-motivation, adaptability, reliability and organization.
